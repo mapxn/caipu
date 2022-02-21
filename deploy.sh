f765d8bc666a0001b7bb50c1d238002bb9389cf1
mapxn <16299874+mapxn@users.noreply.github.com>
2022-02-21 23:30:55 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,11 @@ yarn docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+git init
 git add -A
 git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
+git branch -M gh-pages
+git remote add origin git@github.com:mapxn/caipu.git
 git push -f git@github.com:mapxn/caipu.git gh-pages
-
 
 cd -
